52339b7d12931a42b19e8c7a836271545f46c697
Rahei <Rahei@RAHEIMSPC>
2024-11-18 18:54:00 -0500

    Add project files.

---
+++ week3 challenges/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace week3_challenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input1 = "eye";
+            string input2 = "hannah";
+            Console.WriteLine("enter a word to check if its a palinderome: ");
+            string input3 = Console.ReadLine();
+
+            Console.WriteLine($"is {input1}, a palindrome? {IsPalindrome(input1)}");
+            Console.WriteLine($"Is {input2}, a palindrome? {IsPalindrome(input2)}");
+            Console.WriteLine($"Is {input3}, a palindrome? {IsPalindrome(input3)}");
+        }
+
+
+
+
+        static bool IsPalindrome(string str)
+        {
+
+            char[] charArray = str.ToCharArray();
+
+
+            Array.Reverse(charArray);
+
+
+            string reversedString = new string(charArray);
+
+
+            return str.Equals(reversedString, StringComparison.OrdinalIgnoreCase);
+
+
+        }
+
+    }
+}
+
